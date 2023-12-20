193c80c19475aaebff71ece54b4c211c1f9d53f6
lee93 <dohuny2@naver.com>
2023-12-20 19:34:54 +0900

    진행중(보류) - 직원등록

--- src/main/java/com/behit/employee/dao/EmployeeDAO.java
+++ src/main/java/com/behit/employee/dao/EmployeeDAO.java
@@ -1,5 +1,13 @@
 package com.behit.employee.dao;
 
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
 public interface EmployeeDAO {
+
+	int join(HashMap<String, Object> params);
 
 }
