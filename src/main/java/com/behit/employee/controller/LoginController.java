f364ceff454e856907a2ed7ee23049c88298bf79
Yoon_Ye_Soung <solluna9@naver.com>
2023-12-20 17:09:04 +0900

    로그인 기능 만드는중

--- src/main/java/com/behit/employee/controller/LoginController.java
+++ src/main/java/com/behit/employee/controller/LoginController.java
@@ -1,5 +1,26 @@
 package com.behit.employee.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.behit.employee.service.LoginService;
+
+@Controller
 public class LoginController {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired LoginService service;
+	
+	@PostMapping(value="login.do")
+	public ModelAndView login(@RequestParam String emp_id, @RequestParam String password) {
+		ModelAndView mav = new ModelAndView();
+		
+		return mav; 
+	}
 }
