5b2053dfd5979e91d33d4ef4a6ef1e79f1ccb416
Nicole <ritina21@naver.com>
2024-01-10 17:51:09 +0900

    대시보드 결재리스트 추가 / 조직도에 직책 추가

--- src/main/java/com/behit/profile/service/DashBoardService.java
+++ src/main/java/com/behit/profile/service/DashBoardService.java
@@ -1,10 +1,14 @@
 package com.behit.profile.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.behit.approval.dao.ApprovalDAO;
+import com.behit.approval.dto.ApprovalDTO;
 import com.behit.employee.dto.EmployeeDTO;
 import com.behit.profile.dao.ProfileDAO;
 import com.behit.profile.dto.FileDTO;
@@ -15,12 +19,17 @@ public class DashBoardService {
 	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired ProfileDAO profileDAO;
+	@Autowired ApprovalDAO aproDao;
 	public EmployeeDTO detail(String login_id) {
 		return profileDAO.prodetail(login_id);
 	}
 
 	public FileDTO getPhoto(String login_id) {
 		return profileDAO.getPhoto(login_id);
+	}
+
+	public List<ApprovalDTO> reqAp_list(String login_id) {
+		return aproDao.getApproval_list(login_id);
 	}
 	
 }
